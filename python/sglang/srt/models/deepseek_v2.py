d43051d6f52b248cf7fe0c2c7e59dd586d519a1c
UNIDY <unidy2002@outlook.com>
2025-07-29 15:45:04 +0800

    Adapt to the new fault tolerance API of MXA-EP

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -433,6 +433,7 @@ class DeepseekV2MoE(nn.Module):
         if global_server_args_dict["enable_deepep_moe"]:
             # TODO: 未来将支持 tp < ep
             self.ep_size = get_tensor_model_parallel_world_size()
+            self.ep_rank = get_tensor_model_parallel_rank()
             self.num_experts = (
                 config.n_routed_experts
                 + global_server_args_dict["ep_num_redundant_experts"]
@@ -661,6 +662,16 @@ class DeepseekV2MoE(nn.Module):
         """
         forward_mode = forward_batch.forward_mode
         shared_output = None
+        expert_location_dispatch_info = ExpertLocationDispatchInfo.init_new(
+            layer_id=self.layer_id,
+        )
+        broken_nodes = torch.zeros((self.ep_size,), dtype=torch.int32, device='cuda')
+        gathered_experts = torch.zeros((expert_location_dispatch_info.num_physical_experts,), dtype=torch.int32, device='cuda')
+        avoid_rank = int(os.environ.get("SGLANG_EP_AVOID_RANK", -1))
+        if avoid_rank >= 0:
+            broken_nodes[avoid_rank] = 1
+            num_experts_per_rank = expert_location_dispatch_info.num_physical_experts // self.ep_size
+            gathered_experts[avoid_rank * num_experts_per_rank:(avoid_rank + 1) * num_experts_per_rank] = 1
         if is_non_idle_and_non_empty(forward_mode, hidden_states):
             # router_logits: (num_tokens, n_experts)
             router_logits = self.gate(hidden_states)
@@ -669,9 +680,7 @@ class DeepseekV2MoE(nn.Module):
                 hidden_states,
                 router_logits,
                 num_token_non_padded=forward_batch.num_token_non_padded,
-                expert_location_dispatch_info=ExpertLocationDispatchInfo.init_new(
-                    layer_id=self.layer_id,
-                ),
+                expert_location_dispatch_info=expert_location_dispatch_info,
             )
             logging.info(f"topk_idx: {topk_idx}")
         else:
@@ -696,6 +705,7 @@ class DeepseekV2MoE(nn.Module):
                 hidden_states=hidden_states,
                 topk_idx=topk_idx,
                 topk_weights=topk_weights,
+                broken_nodes=broken_nodes,
                 forward_batch=forward_batch,
             )
         final_hidden_states = self.experts(
@@ -710,10 +720,11 @@ class DeepseekV2MoE(nn.Module):
             forward_batch=forward_batch,
         )
         if self.ep_size > 1:
-            final_hidden_states, gathered_experts = self.deepep_dispatcher.combine(
+            final_hidden_states = self.deepep_dispatcher.combine(
                 hidden_states=final_hidden_states,
                 topk_idx=topk_idx,
                 topk_weights=topk_weights,
+                gathered_experts=gathered_experts,
                 forward_batch=forward_batch,
             )
 
