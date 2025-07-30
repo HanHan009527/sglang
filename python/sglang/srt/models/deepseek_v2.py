594c59dc3cc0710c790c7b79f5f54e985ad9cd57
UNIDY <unidy2002@outlook.com>
2025-07-30 22:14:54 +0800

    debug

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -744,7 +744,7 @@ class DeepseekV2MoE(nn.Module):
                 ]
                 finished_logical_experts[gathered_logical_experts] = 1
                 if i == 0:
-                    print(gathered_experts)
+                    print(expert_location_dispatch_info.partial_physical_to_logical_map)
             if final_hidden_states is None:
                 final_hidden_states = hidden_states
             else:
