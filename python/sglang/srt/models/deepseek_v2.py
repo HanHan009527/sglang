1a92105e6db7d3a0b19d8f29670627bf784d09f2
Hank Han <hanhan7630@outlook.com>
2025-08-01 03:54:39 +0800

    remote log

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -2426,7 +2426,7 @@ class DeepseekV2ForCausalLM(nn.Module):
         self.logits_processor = LogitsProcessor(config)
 
         self.inference_counter = 0
-        self.trigger_at = 20
+        self.trigger_at = int(os.environ.get("SGLANG_AVOID_EP_TRIGGER_AT", 10))
 
         self._routed_experts_weights_of_layer = LazyValue(
             lambda: {
