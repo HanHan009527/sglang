f0fd204822a6784684cfecec2c5a00acfd75d06a
Hank Han <hanhan7630@outlook.com>
2025-08-07 20:57:47 +0800

    delete useless code

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -1655,13 +1655,7 @@ class ModelRunner:
                         next(gen)
                     except StopIteration:
                         break
-                output = self._forward_raw(
-                    forward_batch,
-                    skip_attn_backend_init,
-                    pp_proxy_tensors,
-                    reinit_attn_backend,
-                    split_forward_count,
-                )
+
             if self.eplb_manager is not None:
                 self.eplb_manager.on_forward_pass_end()
 
