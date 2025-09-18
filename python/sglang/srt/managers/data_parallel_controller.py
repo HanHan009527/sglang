910725604d74f66addb55b0266413402de049808
ympcMark <ympc2005@qq.com>
2025-09-18 14:57:58 +0800

    q

--- python/sglang/srt/managers/data_parallel_controller.py
+++ python/sglang/srt/managers/data_parallel_controller.py
@@ -361,6 +361,9 @@ class DataParallelController:
             while True:
                 if self.status[self.round_robin_counter] == 1: 
                     self.workers[self.round_robin_counter].send_pyobj(req)
+                    self.round_robin_counter = (self.round_robin_counter + 1) % len(
+                        self.workers
+                    )
                     break
                 self.round_robin_counter = (self.round_robin_counter + 1) % len(
                     self.workers
