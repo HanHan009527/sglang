920d07c52ff5ecd995d5a74b153acd4dad0d6fa4
ympcMark <ympc2005@qq.com>
2025-09-19 22:13:16 +0800

    feat

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -2061,6 +2061,9 @@ class Scheduler(
                 embeddings=embeddings, bid=model_worker_batch.bid
             )
 
+        print("UPDATE!!!")
+        print(get_tp_active_ranks().tolist())
+        print("END!!!!")
         self.send_to_tokenizer.send_pyobj(Ranks(get_tp_active_ranks().tolist()))
 
         return ret
