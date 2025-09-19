2c3ca289cd8a7c68c145422a799352ebd88b3b56
ympcMark <ympc2005@qq.com>
2025-09-19 21:20:05 +0800

    feat

--- python/sglang/srt/managers/data_parallel_controller.py
+++ python/sglang/srt/managers/data_parallel_controller.py
@@ -186,7 +186,6 @@ class DataParallelController:
 
     def update_ranks(self, ranks: Ranks):
         self.status = ranks.status
-        print(f"update: {self.status}")
 
     def init_dispatcher(self):
         self._request_dispatcher = TypeBasedDispatcher(
